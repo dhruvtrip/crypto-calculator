7f5489d000a76476ff1cdfd5d800d05da021de59
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-01-17 17:35:44 +0000

    added styling and ids

---
+++ script.js
@@ -0,0 +1,7 @@
+let investLink = getElementById('invest-link');
+let investAmount = getElementById('invest-amount');
+let cryptoName = getElementById('crypto-name');
+let cryptoPrice = getElementById('crypto-price');
+let totalAmount = getElementById('total-amount');
+
+
