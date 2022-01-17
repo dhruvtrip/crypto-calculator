127a9091aa454841aaa998cfb07717332c85c702
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-01-17 21:15:25 +0000

    event to change calculator format

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let investLink = getElementById('invest-link');
-let investAmount = getElementById('invest-amount');
-let cryptoName = getElementById('crypto-name');
-let cryptoPrice = getElementById('crypto-price');
-let totalAmount = getElementById('total-amount');
+
+let investLink = document.getElementById('invest-link');
+let investAmount = document.getElementById('invest-amount');
+let cryptoName = document.getElementById('crypto-name');
+let cryptoPrice = document.getElementById('crypto-price');
+let totalAmount = document.getElementById('total-amount');
 
 
+//show the other calculator format
+investLink.addEventListener('click', changeCalculator =>{
+    document.querySelector('p').innerHTML = 'changed text';
+});
+
+//adds user invested amount
+let userAmount = get
+investAmount.addEventListener('click', addUserAmount =>{
+    investAmount.innerHTML = userAmount;
+})
