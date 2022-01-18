438cd806dbd9f95ecaa0b746e85e81544b5fbd46
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-01-18 21:39:08 +0000

    calculator switch added

--- script.js
+++ script.js
@@ -2,17 +2,29 @@
 let investLink = document.getElementById('invest-link');
 let investAmount = document.getElementById('invest-amount');
 let cryptoName = document.getElementById('crypto-name');
+let genericText = document.getElementById('generic-text-1');
 let cryptoPrice = document.getElementById('crypto-price');
 let totalAmount = document.getElementById('total-amount');
 
 
 //show the other calculator format
 investLink.addEventListener('click', changeCalculator =>{
-    document.querySelector('p').innerHTML = 'changed text';
-});
+    if (investLink.innerHTML === "invest") {
+        investLink.innerHTML = "invested";
+        genericText.innerHTML = `on ${addDateFormat()}`; //need to add tag here
+        cryptoPrice.remove();
+      } else {
+        investLink.innerHTML = "invest";
+        genericText.innerHTML = "today and it goes to $";
+        genericText.appendChild(cryptoPrice);
+      }
+}); 
 
-//adds user invested amount
-let userAmount = get
-investAmount.addEventListener('click', addUserAmount =>{
-    investAmount.innerHTML = userAmount;
-})
+//function to add new Date tag - creating new span tag with an existing class 
+
+const addDateFormat = function (){
+    let dateVariable = document.createElement("span");
+    dateVariable.setAttribute('class','highlighted-text');
+    cryptoName.appendChild(dateVariable);
+    console.log(dateVariable)
+};
