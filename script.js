8da568b9f097eedac4ff48a25a4272dd318db7b8
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-01-20 18:26:05 +0000

    api connection established

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ let totalAmount = document.getElementById('total-amount');
 
 //show the other calculator format
 investLink.addEventListener('click', changeCalculator =>{
-    if (investLink.innerHTML === "invest") {
+    //toggle feature
+    if (investLink.innerHTML === "invest") { 
         investLink.innerHTML = "invested";
         genericText.innerHTML = `on ${addDateFormat()}`; //need to add tag here
         cryptoPrice.remove();
@@ -20,11 +21,41 @@ investLink.addEventListener('click', changeCalculator =>{
       }
 }); 
 
+//fetching coin data from coinranking's api - json
+
+fetch("https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&symbols=BTC%2CETH&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+		"x-rapidapi-key": "eea92ee8b8mshf9664ba0f036a02p1cc450jsneb4c624ca4fa"
+	}
+})
+.then(response => {
+	console.log(response.json()
+  .then(response=>{
+    console.log(response)
+  }));
+})
+.catch(err => {
+	console.error(err);
+});
+
+//displaying top three crypto prices
+
+
+
+//add click event on the crypto name tag
+cryptoName.addEventListener('click', showCryptoList=>{
+  console.log("123")
+})
+
+
 //function to add new Date tag - creating new span tag with an existing class 
 
-const addDateFormat = function (){
-    let dateVariable = document.createElement("span");
-    dateVariable.setAttribute('class','highlighted-text');
-    cryptoName.appendChild(dateVariable);
-    console.log(dateVariable)
-};
+// const addDateFormat = function (){
+//     let dateVariable = document.createElement("span");
+//     dateVariable.setAttribute('class','highlighted-text');
+//     cryptoName.appendChild(dateVariable);
+//     console.log(dateVariable)
+// };
+
